9ef68c7408246ae98257a282cd4951e58d4995c7
rflores <rodrico1209@gmail.com>
2022-01-11 16:28:41 -0400

    iniciando proyecto

---
+++ tarea1.js
@@ -0,0 +1,43 @@
+
+
+var nombre = 7
+var apellido = 5
+var result1 = nombre + apellido
+console.log(nombre + ' + ' + apellido + ' = ', result1);
+
+var name = 'rodrigo';
+var name2 = 'flores'
+console.log(  name  + ' ' + name2);
+
+var sum1 = 5;
+var sum2 = 1;
+var sum3 = 2;
+var sum4 = 3;
+var sum5 = 4;
+var sum6 = 5;
+var sum7 = 6;
+var sum8 = 7;
+var sum9 = 8;
+var sum10 = 9;
+var sum11 = 10;
+var sum12 = 0
+var result1 = sum1 * sum2
+var result2 = sum1 * sum3
+var result3 = sum1 * sum4
+var result4 = sum1 * sum5
+var result5 = sum1 * sum6
+var result6 = sum1 * sum7
+var result7 = sum1 * sum8
+var result8 = sum1 * sum9
+var result9 = sum1 * sum10
+var result10 = sum1 / sum12
+console.log(sum1 + ' * ' + sum2 + ' = ', result1);
+console.log(sum1 + ' * ' + sum3 + ' = ', result2);
+console.log(sum1 + ' * ' + sum4 + ' = ', result3);
+console.log(sum1 + ' * ' + sum5 + ' = ', result4);
+console.log(sum1 + ' * ' + sum6 + ' = ', result5);
+console.log(sum1 + ' * ' + sum7 + ' = ', result6);
+console.log(sum1 + ' * ' + sum8 + ' = ', result7);
+console.log(sum1 + ' * ' + sum9 + ' = ', result8);
+console.log(sum1 + ' * ' + sum10 + ' = ', result9);
+console.log(sum1 + ' / ' + sum12+ ' = ', result10);♦
