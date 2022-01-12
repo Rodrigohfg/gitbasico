52f6f41ccfd89f7a0a44257a5a39f7115e3c99dd
rflores <rodrico1209@gmail.com>
2022-01-12 13:01:09 -0400

    tarea6.js

---
+++ tarea6.js
@@ -0,0 +1,40 @@
+const number = 9
+let  resultado = 0
+for (let a = 1; a <11 ; a = a+1) {
+    resultado = number + resultado
+    console.log(number + ' * ' + a +  ' = ' + resultado );
+}
+
+const number1 = 8
+let  resultado1 = 0
+for (let a = 1; a<=3 ; a = a+1) {
+    resultado1 = number1 + resultado1 
+    console.log(number1 + ' * ' + a + ' = ' + resultado1 );
+}
+
+const number2 = 2
+let b = 0
+    resultado2 = 0
+while (b < 6) {
+    resultado2 = number2 + resultado2
+    b = b + 1  
+    console.log( number2 + ' * ' + b + ' = ' + resultado2 );
+}
+
+const number3 = 21
+let  resultado3 = 0
+for (let x = 1; x<2 ; x = x+1) {
+    resultado3 = number3 - resultado3 
+    console.log(number3 + ' / ' + x + ' = ' + resultado3 );
+}
+
+
+const number4 = 2
+let y = 0
+    resultado4 = 0
+while (y < 2) {
+    resultado4 = number4 - resultado4
+    y = y + 1  
+    console.log( number4 + ' / ' + y + ' = ' + resultado4 );
+}
+
